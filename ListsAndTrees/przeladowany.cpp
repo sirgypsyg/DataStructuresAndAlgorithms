0190584d0f72335ab067b1cf4fe7df1fe9f4b14b
kuba <jakubobara1@wp.pl>
2023-11-25 06:20:27 +0100

    Implemented Queue and stack

--- ListsAndTrees/przeladowany.cpp
+++ ListsAndTrees/przeladowany.cpp
@@ -1,11 +1,121 @@
 #include <iostream>
 #include <queue>
-#include <stack>
+
+template <typename T>
+class Stack {
+private:
+    T* elements;    // Tablica dynamiczna do przechowywania elementów
+    size_t capacity; // Pojemność tablicy
+    size_t size;     // Aktualny rozmiar stosu
+
+public:
+    Stack() : capacity(10), size(0) {
+        elements = new T[capacity];
+    }
+
+    ~Stack() {
+        delete[] elements;
+    }
+
+    void push(const T& value) {
+        if (size == capacity) {
+            // Jeżeli stos jest pełny, zwiększ pojemność
+            resize();
+        }
+        elements[size++] = value;
+    }
+
+    void pop() {
+        if (!empty()) {
+            --size;
+        }
+    }
+
+    T top() const {
+        if (!empty()) {
+            return elements[size - 1];
+        } else {
+            throw std::out_of_range("Stack is empty");
+        }
+    }
+
+    bool empty() const {
+        return size == 0;
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+
+private:
+    void resize() {
+        capacity *= 2;
+        T* newElements = new T[capacity];
+        for (size_t i = 0; i < size; ++i) {
+            newElements[i] = elements[i];
+        }
+        delete[] elements;
+        elements = newElements;
+    }
+};
 
 
+class Queue{
+    private:
+        char *tab;
+        int head, tail;
+        int size;
+    public:
+        Queue(){
+            head = tail = 0;
+            size = 100;
+            tab = new char[size];
+        }
 
-std::string infToRPN(std::string string) {
-    std::stack<char> stack;
+        ~Queue(){
+            delete[]tab;
+        }
+
+        bool empty(){
+            return head == tail ? true : false;
+        }
+
+        char pop(){
+            char temp = tab[head];
+            head = (head+1)%size;
+            return temp;
+        }
+
+        void push(char element){
+            tab[tail] = element;
+            tail = (tail+1) % size;
+            if (head == tail)
+            {
+                int i = 0;
+                char *temp_tab = new char[2 * size];
+                do
+                {
+                    temp_tab[i] = tab[head];
+                    head = (head + 1)% size;
+                    ++i;
+                } while (head != tail);
+                
+                delete[] tab;
+                tab = temp_tab;
+                head = 0;
+                tail = size;
+                size *= 2;                
+            }
+        }
+
+        void clear(){
+            head = tail = 0;
+        }
+};
+
+
+void infToRPN(std::string string, Queue &que) {
+    Stack<char> stack;
     int i = 0;
     std::string rpn;
     int size = string.size();
@@ -14,26 +124,30 @@ std::string infToRPN(std::string string) {
         char e = string.at(i);
 
         if (isdigit(e) || isalpha(e))
-            rpn += e;
+            que.push(e);
         else if (e == '[' || e == '{' || e == '(')
             stack.push(e);
         else if (e == ']' || e == '}' || e == ')') {
             while (!stack.empty() && stack.top() != '[' && stack.top() != '{' && stack.top() != '(') {
-                rpn += stack.top();
+                que.push(stack.top());
                 stack.pop();
             }
-            if (stack.empty() || stack.top() != '[' && stack.top() != '{' && stack.top() != '(')
-                return "BLAD";
+            if (stack.empty() || stack.top() != '[' && stack.top() != '{' && stack.top() != '('){
+                que.clear();
+                return;
+            }
             stack.pop();
         } else if (e == '+' || e == '-') {
             if (i < size - 1)
             {
                 char e1 = string.at(i+1);
-                if (e1 == '+' || e1 == '-' || e1 == '/' || e1 == '*')
-                    return "BLAD";
+                if (e1 == '+' || e1 == '-' || e1 == '/' || e1 == '*'){
+                    que.clear();
+                    return;
+                }
             }
             while (!stack.empty() && stack.top() != '[' && stack.top() != '{' && stack.top() != '(') {
-                rpn += stack.top();
+                que.push(stack.top());
                 stack.pop();
             }
             stack.push(e);
@@ -41,12 +155,14 @@ std::string infToRPN(std::string string) {
             if (i < size - 1)
             {
                 char e1 = string.at(i+1);
-                if (e1 == '+' || e1 == '-' || e1 == '/' || e1 == '*')
-                    return "BLAD";
+                if (e1 == '+' || e1 == '-' || e1 == '/' || e1 == '*'){
+                    que.clear();
+                    return;
+                }
             }
             
             while (!stack.empty() && (stack.top() == '+' || stack.top() == '-' ||stack.top() == '*' || stack.top() == '/')) {
-                rpn += stack.top();
+                que.push(stack.top());
                 stack.pop();
             }
             stack.push(e);
@@ -57,14 +173,13 @@ std::string infToRPN(std::string string) {
     while (!stack.empty()) {
         if (stack.top() == '[' || stack.top() == '{' || stack.top() == '(')
         {
-            return "BLAD";
+            que.clear();
+            return;
         }
         
-        rpn += stack.top();
+        que.push(stack.top());
         stack.pop();
     }
-
-    return rpn;
 }
 
 int main() {
@@ -75,16 +190,29 @@ int main() {
     std::cin >> n;
     std::cin.ignore();
     std::string string, rpn;
-    std::string output[n];
+    Queue que[n];
 
     for (int i = 0; i < n; i++) {
         std::getline(std::cin, string);
-        output[i] = infToRPN(string);
+        infToRPN(string,que[i]);
     }
     for (int i = 0; i < n; i++)
     {
-        std::cout << i << " " << output[i] << "\n";
+        if (que[i].empty())
+        {
+            std::cout << i << " " << "BLAD" << "\n";
+            continue;;
+        }
+        else{
+            while (!que[i].empty())
+            {
+                std::cout << i << " " << que[i].pop();
+            }
+        }
+        
+        std::cout << "\n";
     }
+    
 
     return 0;
 }
